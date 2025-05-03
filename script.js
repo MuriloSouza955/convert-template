6e399dfe3e4c51ff424aa392e30f90f3eef5b8ac
MuriloSouza955 <souza.murilo@gmail.com>
2025-05-03 18:04:55 -0300

    Obtendo a moeda selecionada pelo usuário

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
 
 //Manipilando o input amount para receber apenas números
 amount.addEventListener('input', ()=> {
-    amount.value = amount.value.replace(/[^0-9]/g, '');
-
-    
+    amount.value = amount.value.replace(/[^0-9]/g, '');    
 })
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    
+    console.log(currency.value)
+}
